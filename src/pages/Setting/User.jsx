3f10a3e3dd1df3ce590c483f19bdc9a8410966de
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-03-20 10:04:38 +0700

    update setting

--- src/pages/Setting/User.jsx
+++ src/pages/Setting/User.jsx
@@ -1,33 +1,45 @@
 import React from 'react'
 import "./Settings.css"
+import {useRef} from 'react';
+
+
 
 const Avatar = () => {
+  const inputRef = useRef(null);
+
+
+  const handleClick = () => {
+    // 👇️ open file input box on click of another element
+    inputRef.current.click();
+  };
+
+  const handleFileChange = event => {
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj) {
+      return;
+    }
+
+    console.log('fileObj is', fileObj);
+
+    // 👇️ reset file input
+    event.target.value = null;
+
+    // 👇️ is now empty
+    console.log(event.target.files);
+
+    // 👇️ can still access file object here
+    console.log(fileObj);
+    console.log(fileObj.name);
+  };
   return (
     <div className='setavatar'>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYmlp9JDeNMaFZzw9S3G1dVztGqF_2vq9nA&usqp=CAU" />
-        <label>
-          <input
-            type="file" 
-            name="avatar"
-          />
-        </label>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYmlp9JDeNMaFZzw9S3G1dVztGqF_2vq9nA&usqp=CAU" />
+        <input ref={inputRef} hidden type="file" onChange={handleFileChange}/>
+        <button onClick={handleClick}>Choose File</button>
     </div>
   )
 }
 
-const AvaSetting = () => {
-  return (
-    <div className='avaSetting'>
-      <Avatar />
-        <label>
-          <button type="submit" name="avatar" className='upload'>Upload
-          </button>
-          <button type="submit" name="avatar">Cancel
-          </button>
-        </label>
-    </div>
-  )
-}
 
 const InputArea = ({type,label,name, placeholder}) =>{
   return (
@@ -44,7 +56,7 @@ const User = () => {
     <div className='user padding-wrapper'>
       <h1 className="page-title">Settings</h1>
       <form className='form'>
-        <AvaSetting />
+        <Avatar />
         <hr></hr>
         <InputArea type="text" label = "Username" name = "Username" placeholder="Username"/>
         <hr></hr>
