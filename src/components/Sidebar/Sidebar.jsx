fac96d5c0559b3674b3a3f5401af8a309ea3354c
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-03-22 18:16:08 +0700

    update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,128 +1,129 @@
-import React from 'react'
-import './Sidebar.css'
-import {Link, useLocation} from "react-router-dom"
-import { useState } from "react";
+  import React from 'react'
+  import './Sidebar.css'
+  import {Link, useLocation} from "react-router-dom"
+  import { useState } from "react";
 
-import {
-  AiOutlineLogout,
-  AiFillSetting,
-} from "react-icons/ai"
-import {
-  MdDashboard,
-  MdIntegrationInstructions,
-  MdLightbulb,
-  MdWaterDrop
-} from "react-icons/md"
-import {
-  FaHistory,
-  FaUser,
-  FaLock
-} from "react-icons/fa"
+  import {
+    AiOutlineLogout,
+    AiFillSetting,
+  } from "react-icons/ai"
+  import {
+    MdDashboard,
+    MdIntegrationInstructions,
+    MdLightbulb,
+    MdWaterDrop
+  } from "react-icons/md"
+  import {
+    FaHistory,
+    FaUser,
+    FaLock
+  } from "react-icons/fa"
 
 
 
-const Logo = () => {
-  return (
-    <div className="logo">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/HCMUT_official_logo.png" />
-      <h4><b>SMART FARM</b></h4>
-    </div>
-  );
-};
-
-const Avatar = () => {
-  return (
-    <div className='avatar'>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYmlp9JDeNMaFZzw9S3G1dVztGqF_2vq9nA&usqp=CAU" />
-      <div className='avatar_info'>
-        <h4><b>Push in boots</b></h4>
-        <h5>Administrator</h5>
+  const Logo = () => {
+    return (
+      <div className="logo">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/HCMUT_official_logo.png" />
+        <h4><b>SMART FARM</b></h4>
       </div>
-    </div>
-  )
-}
+    );
+  };
 
-const SideBarItem = ({ Item, page, href }) => {
-  const location = useLocation();
-
-  // Check if the current path matches the `href` prop
-  const isActive = location.pathname === href;
-
-  return (
-    <>
-      <Link className="sidebar_link" to={href}>
-        <div className={isActive ? "sidebar_item active" : "sidebar_item"}>
-          {<Item className="sidebar_img" size={".8em"} />}
-          <h3 className={isActive ? "active" : ""}>
-            {page}
-          </h3>
+  const Avatar = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    return (
+      <div className='avatar'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYmlp9JDeNMaFZzw9S3G1dVztGqF_2vq9nA&usqp=CAU" />
+        <div className='avatar_info'>
+          <h4><b>{storedUser.username}</b></h4>
+          <h5>Administrator</h5>
         </div>
-      </Link>
-    </>
-  );
-};
-const SubSideBarItem = ({ Item, page,onHover, href, SubItem1, Subpage1, Subhref1, SubItem2, Subpage2, Subhref2 }) => {
-  const location = useLocation();
-
-  // Check if the current path matches the `href` prop
-  const isActive = location.pathname === href;
-  return (
-    <>
-      <Link className="sub_sidebar_link" to={href} >
-        <div className={isActive ? "sub_sidebar_item active" : "sub_sidebar_item"}>
-          {<Item className="sidebar_img" />}
-          <h3>
-            {page}
-          </h3>
-        </div>
-      </Link>
-      <div className='devices_popout' /*style={onHover ? { display: "block" } : { display: "none" }}*/>
-        <SideBarItem Item={SubItem1} page={Subpage1} href={Subhref1} />
-        <SideBarItem Item={SubItem2} page={Subpage2} href={Subhref2} />
       </div>
-    </>
-  );
-};
+    )
+  }
 
-const Logout = () => {
-  return (
-    <div className='logout'>
-      <SideBarItem Item={AiOutlineLogout} page="Logout" href="/" />
-    </div>
-  )
-}
+  const SideBarItem = ({ Item, page, href }) => {
+    const location = useLocation();
 
+    // Check if the current path matches the `href` prop
+    const isActive = location.pathname === href;
 
-const Sidebar = () => {
-  const [onHover, setOnHover] = useState(false);
-  const [onHover2, setOnHover2] = useState(false);
-  return (
-    <div>
-      <div className='bground'>
-        <div className='sidebar'>
-          <Logo/>
-          <Avatar/>
-          <div className='sidebar_container'>
-            <SideBarItem Item={MdDashboard} page="Dashboard" href="/" id="dashboard" />
-            <div onMouseOver={() => setOnHover(true)} onMouseOut={() => setOnHover(false)}>
-            <SubSideBarItem Item={MdIntegrationInstructions} page="Devices" href="/devices" onHover={onHover} id="device"
-                            SubItem1={MdLightbulb} Subpage1="Light" Subhref1="/devices/lights"
-                            SubItem2={MdWaterDrop} Subpage2="Water Pump" Subhref2="/devices/water-pumps"
-                            />
-            </div>
-            <SideBarItem Item={FaHistory} page="History Logs" href="/log" id="log"/>
-            <div onMouseOver={() => setOnHover2(true)} onMouseOut={() => setOnHover2(false)}>
-            <SubSideBarItem Item={AiFillSetting} page="Setting" href="/setting" onHover={onHover2} id="setting"
-                            SubItem1={FaUser} Subpage1="Personal Infomation" Subhref1="/user"
-                            SubItem2={FaLock} Subpage2="Password" Subhref2="/password"
-                            />
-            </div>
+    return (
+      <>
+        <Link className="sidebar_link" to={href}>
+          <div className={isActive ? "sidebar_item active" : "sidebar_item"}>
+            {<Item className="sidebar_img" size={".8em"} />}
+            <h3 className={isActive ? "active" : ""}>
+              {page}
+            </h3>
           </div>
-          <Logout/>
+        </Link>
+      </>
+    );
+  };
+  const SubSideBarItem = ({ Item, page,onHover, href, SubItem1, Subpage1, Subhref1, SubItem2, Subpage2, Subhref2 }) => {
+    const location = useLocation();
+
+    // Check if the current path matches the `href` prop
+    const isActive = location.pathname === href;
+    return (
+      <>
+        <Link className="sub_sidebar_link" to={href} >
+          <div className={isActive ? "sub_sidebar_item active" : "sub_sidebar_item"}>
+            {<Item className="sidebar_img" />}
+            <h3>
+              {page}
+            </h3>
+          </div>
+        </Link>
+        <div className='devices_popout' /*style={onHover ? { display: "block" } : { display: "none" }}*/>
+          <SideBarItem Item={SubItem1} page={Subpage1} href={Subhref1} />
+          <SideBarItem Item={SubItem2} page={Subpage2} href={Subhref2} />
+        </div>
+      </>
+    );
+  };
+
+  const Logout = () => {
+    return (
+      <div className='logout'>
+        <SideBarItem Item={AiOutlineLogout} page="Logout" href="/" />
+      </div>
+    )
+  }
+
+
+  const Sidebar = () => {
+    const [onHover, setOnHover] = useState(false);
+    const [onHover2, setOnHover2] = useState(false);
+    return (
+      <div>
+        <div className='bground'>
+          <div className='sidebar'>
+            <Logo/>
+            <Avatar/>
+            <div className='sidebar_container'>
+              <SideBarItem Item={MdDashboard} page="Dashboard" href="/" id="dashboard" />
+              <div onMouseOver={() => setOnHover(true)} onMouseOut={() => setOnHover(false)}>
+              <SubSideBarItem Item={MdIntegrationInstructions} page="Devices" href="/devices" onHover={onHover} id="device"
+                              SubItem1={MdLightbulb} Subpage1="Light" Subhref1="/devices/lights"
+                              SubItem2={MdWaterDrop} Subpage2="Water Pump" Subhref2="/devices/water-pumps"
+                              />
+              </div>
+              <SideBarItem Item={FaHistory} page="History Logs" href="/log" id="log"/>
+              <div onMouseOver={() => setOnHover2(true)} onMouseOut={() => setOnHover2(false)}>
+              <SubSideBarItem Item={AiFillSetting} page="Setting" href="/setting" onHover={onHover2} id="setting"
+                              SubItem1={FaUser} Subpage1="Personal Infomation" Subhref1="/user"
+                              SubItem2={FaLock} Subpage2="Password" Subhref2="/password"
+                              />
+              </div>
+            </div>
+            <Logout/>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default Sidebar
+  export default Sidebar
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './Dashboard.css'
+import './Dashboard.css';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,10 +11,9 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+import useWebSocket from 'react-use-websocket';
 import moment from 'moment';
-import faker from 'faker'
-
+import faker from 'faker';
 
 ChartJS.register(
   CategoryScale,
@@ -40,57 +39,57 @@ const light = {
   },
 };
 const temperature = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'TEMPERATURE',
-      },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
     },
-  };
-  const humidity = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'HUMIDITY',
-      },
+    title: {
+      display: true,
+      text: 'TEMPERATURE',
     },
-  };
-  const moisture = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'MOISTURE',
-      },
+  },
+};
+const humidity = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
     },
-  };
-  const gdd = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'GDD',
-      },
+    title: {
+      display: true,
+      text: 'HUMIDITY',
     },
-  };
+  },
+};
+const moisture = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
+    },
+    title: {
+      display: true,
+      text: 'MOISTURE',
+    },
+  },
+};
+const gdd = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
+    },
+    title: {
+      display: true,
+      text: 'GDD',
+    },
+  },
+};
 
 const labels = [];
 
@@ -106,7 +105,6 @@ const data = {
   ],
 };
 
-
 const Dashboard = () => {
   const [chartData, setChartData] = useState({
     light: data,
@@ -116,38 +114,41 @@ const Dashboard = () => {
     gdd: data,
   });
 
+  const { lastJsonMessage } = useWebSocket('ws://localhost:5000');
+
   useEffect(() => {
-    const fetchChartData = async () => {
-      try {
-        const feedKeys = ['light', 'temperature', 'moisture', 'humidity', 'gdd'];
-        const dataRequests = feedKeys.map((key) => axios.get(`http://localhost:5000/api/adafruit/${key}`));
-        const responses = await Promise.all(dataRequests);
-        const chartDataUpdates = responses.reduce((acc, { data: responseData }, index) => {
-          const labels = responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.created_at.substring(11,19));
-          acc[feedKeys[index]] = {
+    if (lastJsonMessage) {
+      const { type, data } = lastJsonMessage;
+      if (type === 'adafruit') {
+        setChartData((prevState) => {
+          const newChartData = { ...prevState };
+          const feedKey = data.feed;
+          const feedData = data.data;
+          const labels = feedData
+            .filter((feed, index) => index % 5 === 0)
+            .reverse()
+            .map((feed) => feed.created_at.substring(11, 19));
+          const newDataset = {
+            ...data.datasets[0],
+            data: feedData
+              .filter((feed, index) => index % 5 === 0)
+              .reverse()
+              .map((feed) => feed.value),
+          };
+          newChartData[feedKey] = {
             ...data,
             labels,
-            datasets: [
-              {
-                ...data.datasets[0],
-                data: responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.value),
-              },
-            ],
+            datasets: [newDataset],
           };
-          return acc;
-        }, {});
-        setChartData(chartDataUpdates);
-      } catch (error) {
-        console.error(error);
+          return newChartData;
+        });
       }
-    };
-    const interval = setInterval(fetchChartData, 0);
-    return () => clearInterval(interval);
-  }, []);
+    }
+  }, [lastJsonMessage]);
 
   return (
     <div className='dashboard padding-wrapper'>
-      <h1 className="page-title">Dashboard</h1>
+      <h1 className='page-title'>Dashboard</h1>
       <div className='DB'>
         <div className='row'>
           <div className='holder'>
@@ -177,4 +178,191 @@ const Dashboard = () => {
 
 
 export default Dashboard
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import './Dashboard.css'
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import { Line } from 'react-chartjs-2';
+// import axios from 'axios';
+// import moment from 'moment';
+// import faker from 'faker'
+
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// const light = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+//       position: 'top',
+//       display: false,
+//     },
+//     title: {
+//       display: true,
+//       text: 'LIGHT',
+//     },
+//   },
+// };
+// const temperature = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//         display: false,
+//       },
+//       title: {
+//         display: true,
+//         text: 'TEMPERATURE',
+//       },
+//     },
+//   };
+//   const humidity = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//         display: false,
+//       },
+//       title: {
+//         display: true,
+//         text: 'HUMIDITY',
+//       },
+//     },
+//   };
+//   const moisture = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//         display: false,
+//       },
+//       title: {
+//         display: true,
+//         text: 'MOISTURE',
+//       },
+//     },
+//   };
+//   const gdd = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//         display: false,
+//       },
+//       title: {
+//         display: true,
+//         text: 'GDD',
+//       },
+//     },
+//   };
+
+// const labels = [];
+
+// const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: '',
+//       data: [],
+//       borderColor: '#C8DCC6',
+//       backgroundColor: '#a0e09b',
+//     },
+//   ],
+// };
+
+
+// const Dashboard = () => {
+//   const [chartData, setChartData] = useState({
+//     light: data,
+//     temperature: data,
+//     moisture: data,
+//     humidity: data,
+//     gdd: data,
+//   });
+
+//   useEffect(() => {
+//     const fetchChartData = async () => {
+//       try {
+//         const feedKeys = ['light', 'temperature', 'moisture', 'humidity', 'gdd'];
+//         const dataRequests = feedKeys.map((key) => axios.get(`http://localhost:5000/api/adafruit/${key}`));
+//         const responses = await Promise.all(dataRequests);
+//         const chartDataUpdates = responses.reduce((acc, { data: responseData }, index) => {
+//           const labels = responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.created_at.substring(11,19));
+//           acc[feedKeys[index]] = {
+//             ...data,
+//             labels,
+//             datasets: [
+//               {
+//                 ...data.datasets[0],
+//                 data: responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.value),
+//               },
+//             ],
+//           };
+//           return acc;
+//         }, {});
+//         setChartData(chartDataUpdates);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     const interval = setInterval(fetchChartData, 0);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div className='dashboard padding-wrapper'>
+//       <h1 className="page-title">Dashboard</h1>
+//       <div className='DB'>
+//         <div className='row'>
+//           <div className='holder'>
+//             <Line options={light} data={chartData.light} />
+//           </div>
+//           <div className='holder'>
+//             <Line options={temperature} data={chartData.temperature} />
+//           </div>
+//         </div>
+//         <div className='row'>
+//           <div className='holder'>
+//             <Line options={moisture} data={chartData.moisture} />
+//           </div>
+//           <div className='holder'>
+//             <Line options={humidity} data={chartData.humidity} />
+//           </div>
+//         </div>
+//         <div className='row'>
+//           <div className='holder'>
+//             <Line options={gdd} data={chartData.gdd} />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+// export default Dashboard
 
