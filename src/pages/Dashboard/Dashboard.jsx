15687bfccd6beddc6998eae1d53b5c6119c37061
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-04-18 12:32:30 +0700

    update dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,186 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import './Dashboard.css';
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from 'chart.js';
-// import { Line } from 'react-chartjs-2';
-// import useWebSocket from 'react-use-websocket';
-// import moment from 'moment';
-// import faker from 'faker';
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// const light = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       position: 'top',
-//       display: false,
-//     },
-//     title: {
-//       display: true,
-//       text: 'LIGHT',
-//     },
-//   },
-// };
-// const temperature = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       position: 'top',
-//       display: false,
-//     },
-//     title: {
-//       display: true,
-//       text: 'TEMPERATURE',
-//     },
-//   },
-// };
-// const humidity = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       position: 'top',
-//       display: false,
-//     },
-//     title: {
-//       display: true,
-//       text: 'HUMIDITY',
-//     },
-//   },
-// };
-// const moisture = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       position: 'top',
-//       display: false,
-//     },
-//     title: {
-//       display: true,
-//       text: 'MOISTURE',
-//     },
-//   },
-// };
-// const gdd = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       position: 'top',
-//       display: false,
-//     },
-//     title: {
-//       display: true,
-//       text: 'GDD',
-//     },
-//   },
-// };
-
-// const labels = [];
-
-// const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: '',
-//       data: [],
-//       borderColor: '#C8DCC6',
-//       backgroundColor: '#a0e09b',
-//     },
-//   ],
-// };
-
-// const Dashboard = () => {
-//   const [chartData, setChartData] = useState({
-//     light: data,
-//     temperature: data,
-//     moisture: data,
-//     humidity: data,
-//     gdd: data,
-//   });
-
-//   const { lastJsonMessage } = useWebSocket('wss://smart-farm-be.onrender.com');
-
-//   useEffect(() => {
-//     if (lastJsonMessage) {
-//       console.log(lastJsonMessage);
-//       const { feed_key, value, created_at } = lastJsonMessage;
-//       const chartDataUpdates = {
-//         ...chartData,
-//         [feed_key]: {
-//           ...data,
-//           labels: [...chartData[feed_key].labels, moment(created_at).format('HH:mm:ss')],
-//           datasets: [
-//             {
-//               ...data.datasets[0],
-//               data: [...chartData[feed_key].datasets[0].data, value],
-//             },
-//           ],
-//         },
-//       };
-//       setChartData(chartDataUpdates);
-//     }
-//   }, [lastJsonMessage]);
-
-//   return (
-//     <div className='dashboard padding-wrapper'>
-//       <h1 className='page-title'>Dashboard</h1>
-//       <div className='DB'>
-//         <div className='row'>
-//           <div className='holder'>
-//             <Line options={light} data={chartData.light} />
-//           </div>
-//           <div className='holder'>
-//             <Line options={temperature} data={chartData.temperature} />
-//           </div>
-//         </div>
-//         <div className='row'>
-//           <div className='holder'>
-//             <Line options={moisture} data={chartData.moisture} />
-//           </div>
-//           <div className='holder'>
-//             <Line options={humidity} data={chartData.humidity} />
-//           </div>
-//         </div>
-//         <div className='row'>
-//           <div className='holder'>
-//             <Line options={gdd} data={chartData.gdd} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
-
-
-
-
-
-
-
-
-
-
-/*=====Get data from APIs=====*/
 import React, { useEffect, useState } from 'react';
-import './Dashboard.css'
+import './Dashboard.css';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -192,10 +11,9 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+import useWebSocket from 'react-use-websocket';
 import moment from 'moment';
-import faker from 'faker'
-
+import axios from 'axios';
 
 ChartJS.register(
   CategoryScale,
@@ -221,57 +39,57 @@ const light = {
   },
 };
 const temperature = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'TEMPERATURE',
-      },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
     },
-  };
-  const humidity = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'HUMIDITY',
-      },
+    title: {
+      display: true,
+      text: 'TEMPERATURE',
     },
-  };
-  const moisture = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'MOISTURE',
-      },
+  },
+};
+const humidity = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
     },
-  };
-  const gdd = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'GDD',
-      },
+    title: {
+      display: true,
+      text: 'HUMIDITY',
     },
-  };
+  },
+};
+const moisture = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
+    },
+    title: {
+      display: true,
+      text: 'MOISTURE',
+    },
+  },
+};
+const gdd = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false,
+    },
+    title: {
+      display: true,
+      text: 'GDD',
+    },
+  },
+};
 
 const labels = [];
 
@@ -282,11 +100,10 @@ const data = {
       label: '',
       data: [],
       borderColor: '#C8DCC6',
-      backgroundColor: '#a0e09b',
+      backgroundColor: '#58c750',
     },
   ],
 };
-
 
 const Dashboard = () => {
   const [chartData, setChartData] = useState({
@@ -298,37 +115,129 @@ const Dashboard = () => {
   });
 
   useEffect(() => {
-    const fetchChartData = async () => {
-      try {
-        const feedKeys = ['light', 'temperature', 'moisture', 'humidity', 'gdd'];
-        const dataRequests = feedKeys.map((key) => axios.get(`http://localhost:5000/api/adafruit/${key}`));
-        const responses = await Promise.all(dataRequests);
-        const chartDataUpdates = responses.reduce((acc, { data: responseData }, index) => {
-          const labels = responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.created_at.substring(11,19));
-          acc[feedKeys[index]] = {
-            ...data,
-            labels,
-            datasets: [
-              {
-                ...data.datasets[0],
-                data: responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.value),
-              },
-            ],
-          };
-          return acc;
-        }, {});
-        setChartData(chartDataUpdates);
-      } catch (error) {
-        console.error(error);
+        const fetchChartData = async () => {
+          try {
+            const feedKeys = ['light', 'temperature', 'moisture', 'humidity', 'gdd'];
+            const dataRequests = feedKeys.map((key) => axios.get(`http://localhost:5000/api/adafruit/${key}`));
+            const responses = await Promise.all(dataRequests);
+            const chartDataUpdates = responses.reduce((acc, { data: responseData }, index) => {
+              const labels = responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.created_at.substring(11,19));
+              acc[feedKeys[index]] = {
+                ...data,
+                labels,
+                datasets: [
+                  {
+                    ...data.datasets[0],
+                    data: responseData.filter((feed, index) => index % 5 === 0).reverse().map((feed) => feed.value),
+                  },
+                ],
+              };
+              return acc;
+            }, {});
+            setChartData(chartDataUpdates);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        fetchChartData();
+      }, []);
+
+
+  const { lastJsonMessage } = useWebSocket('ws://localhost:5000');
+  useEffect(() => {
+    if (lastJsonMessage) {
+      const { feed_key, value, created_at } = lastJsonMessage;
+      console.log(lastJsonMessage)
+      const newData = {
+        x: moment(created_at).format('HH:mm:ss'),
+        y: parseFloat(value),
+      };
+      switch (feed_key) {
+        case 'light':
+          setChartData((prevState) => ({
+            ...prevState,
+            light: {
+              ...prevState.light,
+              labels: [...prevState.light.labels, newData.x],
+              datasets: [
+                {
+                  data: [...prevState.light.datasets[0].data, newData.y],
+                  ...prevState.light.datasets,
+                },
+              ],
+            },
+          }));
+          break;
+        case 'temperature':
+          setChartData((prevState) => ({
+            ...prevState,
+            temperature: {
+              ...prevState.temperature,
+              labels: [...prevState.temperature.labels, newData.x],
+              datasets: [
+                {
+                  data: [
+                    ...prevState.temperature.datasets[0].data,
+                    newData.y,
+                  ],
+                  ...prevState.temperature.datasets,
+                },
+              ],
+            },
+          }));
+          break;
+        case 'moisture':
+          setChartData((prevState) => ({
+            ...prevState,
+            moisture: {
+              ...prevState.moisture,
+              labels: [...prevState.moisture.labels, newData.x],
+              datasets: [
+                {
+                  data: [...prevState.moisture.datasets[0].data, newData.y],
+                  ...prevState.moisture.datasets,
+                },
+              ],
+            },
+          }));
+          break;
+        case 'humidity':
+          setChartData((prevState) => ({
+            ...prevState,
+            humidity: {
+              ...prevState.humidity,
+              labels: [...prevState.humidity.labels, newData.x],
+              datasets: [
+                {
+                  data: [...prevState.humidity.datasets[0].data, newData.y],
+                  ...prevState.humidity.datasets,
+                },
+              ],
+            },
+          }));
+          break;
+        case 'gdd':
+          setChartData((prevState) => ({
+            ...prevState,
+            gdd: {
+              ...prevState.gdd,
+              labels: [...prevState.gdd.labels, newData.x],
+              datasets: [
+                {
+                  data: [...prevState.gdd.datasets[0].data, newData.y],
+                  ...prevState.gdd.datasets,
+                },
+              ],
+            },
+          }));
+          break;
       }
-    };
-    const interval = setInterval(fetchChartData, 0);
-    return () => clearInterval(interval);
-  }, []);
+    }
+  }, [lastJsonMessage]);
 
   return (
     <div className='dashboard padding-wrapper'>
-      <h1 className="page-title">Dashboard</h1>
+      <h1 className='page-title'>Dashboard</h1>
       <div className='DB'>
         <div className='row'>
           <div className='holder'>
@@ -353,8 +262,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default Dashboard
+export default Dashboard;
